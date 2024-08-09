e799d2944f19b908a87dd6ae768290c234737697
porkieeeeeee <porkieeeeeee@gmail.com>
2024-08-09 16:14:27 +0900

    feat::resolve 경로 설정

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,15 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import cesium from "vite-plugin-cesium";
 
-// https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react(), cesium()],
+    resolve: {
+        alias: [
+            { find: "@components", replacement: "/src/components" },
+            {
+                find: "@",
+                replacement: "/src",
+            },
+        ],
+    },
 });
